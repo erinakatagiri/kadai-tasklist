e6cf521f354b25fd4dafb6df153eefaad5184e39
erina.katagiri <erina.katagiri@rakuten.com>
2018-06-01 06:00:21 +0000

    bootstrap design

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -5,17 +5,20 @@
 <!-- Write content for each page here -->
 <h1>タスク新規作成ページ</h1>
 
+<div class="row">
+        <div class="col-xs-6">
     {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
 
          {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
         
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
 
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
  
-
+   </div>
+    </div>
 @endsection
--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -5,13 +5,25 @@
 <!-- Write content for each page here -->
 <h1>id = {{ $tasklist->id }} のタスクの詳細ページ</h1>
 
-     <p>ステータス: {{ $tasklist->stauts }}</p>
-    <p>タスク{{ $tasklist->content }}</p>
+      <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasklist->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $tasklist->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $tasklist->content }}</td>
+        </tr>
+    </table>
     
-     {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id]) !!}
+     {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id], ['class' => 'btn btn-default']) !!}
      
       {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
